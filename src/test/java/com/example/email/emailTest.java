899e245a9e5b993f1d07dfe876e344ebbdb0bb7a
vaano94 <vaano94@yahoo.com>
2016-06-19 12:34:20 +0300

    mockito test reworked

--- src/test/java/com/example/email/emailTest.java
+++ src/test/java/com/example/email/emailTest.java
@@ -6,12 +6,16 @@ import com.example.Messaging.SendHTMLEmail;
 import com.example.RegApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.mail.MessagingException;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RegApplication.class)
@@ -23,6 +27,7 @@ public class emailTest {
 
     @Test
     public void EmailSendWithMessageTest() {
+        sender = Mockito.mock(SendHTMLEmail.class);
         User user = new User();
         user.setEmail("vaano94@yahoo.com");
         user.setPassword("v123123!!12");
@@ -32,18 +37,18 @@ public class emailTest {
         mailMessage.setSubject("3MonthJavaJunior");
 
         sender.sendMessage(mailMessage, user);
-
+        verify(sender, times(1)).sendMessage(mailMessage,user);
 
     }
 
-    @Test(expected = NullPointerException.class)
+    /*@Test(expected = NullPointerException.class)
     public void EmailSentWithNullUserTest()
     {
         User user = null;
         MailMessage mailMessage = new MailMessage ();
         mailMessage.setAuthor (user.getEmail ());
         sender.sendMessage (mailMessage,user);
-    }
+    }*/
 
     /*@Test(expected = MessagingException.class)
     public void EmailSentWithWrongUserTest() {
